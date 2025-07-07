d3adcede40860994ba777f6d836b85b0f4240443
JobertDev27 <jobertdev27@gmail.com>
2025-07-07 22:36:41 +0800

    fix: sprint is now perfect
    stamina is waiting  3 sec before recharging and the timer starts only when needed

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -4,14 +4,21 @@ extends CharacterBody3D
 @onready var camera: Camera3D = $Head/Camera3D
 @onready var label: Label = $Control/Label
 @onready var stamina_bar: ProgressBar = $Control/StaminaBar
+@onready var sprint_regen: Timer = $SprintRegen
 
 const JUMP_VELOCITY : float = 4.5
 const MAX_STAMINA : float = 100.0
+const MAX_SPEED : int = 8
+const MIN_SPEED : int = 2
+const AVG_SPEED : int = 4
 
-var speed : int = 5
+var DELTA_TIME : float
+
+var speed : int = AVG_SPEED
 var current_stamina : float = MAX_STAMINA
 var is_sprinting : bool = false
 var can_sprint : bool = true
+var is_stamina_regen : bool = false
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -25,16 +32,15 @@ func _input(event: InputEvent) -> void:
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-70), deg_to_rad(70))
 
 func _physics_process(delta: float) -> void:
+	DELTA_TIME = get_process_delta_time()
 	handle_movement(delta)
 	move_and_slide()
 
 func handle_movement(delta: float) -> void:
-	label.text = "speed: " + str(speed)
+	label.text = "speed: " + str(sprint_regen.time_left)
 	# Debug: end game
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit()
-	
-	handle_sprint()
 	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -55,8 +61,12 @@ func handle_movement(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 		velocity.z = move_toward(velocity.z, 0, speed)
+		is_sprinting = false
+	
+	handle_sprint()
 
 func handle_sprint() -> void:
+	var was_sprinting : bool
 	stamina_bar.value = current_stamina
 	current_stamina = clampf(current_stamina, 0, MAX_STAMINA)
 	
@@ -66,10 +76,24 @@ func handle_sprint() -> void:
 	elif current_stamina == MAX_STAMINA:
 		can_sprint = true
 	
+	if is_stamina_regen:
+		current_stamina += 10 * DELTA_TIME
+	
+	if Input.is_action_just_released("sprint"):
+		was_sprinting = true
+	
 	if is_sprinting:
 		if can_sprint:
-			speed = 15
-			current_stamina -= 1
+			speed = MAX_SPEED
+			is_stamina_regen = false
+			was_sprinting = false
+			current_stamina -= 20 * DELTA_TIME
 	else:
-		speed = 5
-		current_stamina += 1
+		speed = AVG_SPEED
+		if sprint_regen.is_stopped() && was_sprinting:
+			sprint_regen.start()
+			was_sprinting = false
+
+
+func _on_sprint_regen_timeout() -> void:
+	is_stamina_regen = true
